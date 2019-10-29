bbe7359d1f7b0b3add965ff988fdf5c15d6524ec
GONZALO SANTAMARIA DE PAREDES ECHEGUREN <w140156@ccfi.fi.upm.es>
2019-10-29 15:58:58 +0100

    Codigo comentado y tests implementados

--- src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
+++ src/main/java/es/upm/grise/profundizacion2019/ex1/ClaseExamen.java
@@ -4,19 +4,26 @@ public class ClaseExamen {
 	
 	public float metodoAProbar(int a, int b) {
 		
+		// Nodo 0
 		float result = 0;
 		
+		// Nodo 1
 		while( a > b) {
+			// Nodo 
 			a--;
 		}
 		
+		// Nodo 3
 		if (a <= b) {
+			// Nodo 4
 			result = 0;
 		}
 		else {
+			// Nodo 5
 			result = b;
 		}
 		
+		// Nodo 6
 		return result;
 	}
 
--- src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
+++ src/test/java/es/upm/grise/profundizacion2019/ex1/ClaseExamenTest.java
@@ -6,9 +6,22 @@ import org.junit.Test;
 
 public class ClaseExamenTest {
 
+	ClaseExamen pruebas = new ClaseExamen();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void path1() { // 0-1-2-3-4-6
+		int x = 2;
+		int y = 1;
+		assertEquals(pruebas.metodoAProbar(x, y),0,0);
 	}
+	
+	@Test
+	public void path2() { // 0-1-3-4-6
+		int x = 1;
+		int y = 1;
+		assertEquals(pruebas.metodoAProbar(x, y),0,0);
+	}
+	
+	// No se puede realizar el tercer test que pasa por el nodo 5
 
 }
